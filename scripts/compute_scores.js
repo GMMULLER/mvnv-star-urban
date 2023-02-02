fd9965bfdd67d2b385a5d12634fa5ea28b7438b4
Gustavo Moreira <gmorei3@uic.edu>
2023-02-02 13:09:15 -0600

    Including all tags

--- scripts/compute_scores.js
+++ scripts/compute_scores.js
@@ -1,4 +1,4 @@
-function create_panel(allScores){
+function create_panel(allScores, optionsObject, wizard_panel_select){
 
   tooltipDict={
     size:'Small: <100 nodes  Medium: <1000 nodes Large: >1000 nodes',
@@ -10,127 +10,125 @@ function create_panel(allScores){
     structure:'CLUSTER: a set of well connected nodes, such as a community in social networks. NETWORK: the entire network or a subset that is not limited to a specific structure'
   }
 
-let groups = d3.select('#wizard_panel').selectAll('div').data(Object.keys(optionsObject));
+  let groups = d3.select(wizard_panel_select).selectAll('div').data(Object.keys(optionsObject));
 
-let groupsEnter = groups.enter().append('div').attr('class','dataDiv');
+  let groupsEnter = groups.enter().append('div').attr('class','dataDiv');
 
-//Add Header
-let h4 = groupsEnter.append('h4');
+  //Add Header
+  let h4 = groupsEnter.append('h4');
 
-h4.append('span').attr('id','header');
-h4
-.filter(d=>tooltipDict[d])
-.append('span')
-.attr('id','icon')
-.append('i');
+  h4.append('span').attr('id','header');
+  h4
+  .filter(d=>tooltipDict[d])
+  .append('span')
+  .attr('id','icon')
+  .append('i');
 
-let switchEl = h4.append('span').attr('class','field is-pulled-right');
+  let switchEl = h4.append('span').attr('class','field is-pulled-right');
 
-switchEl.append('input')
-.attr('type','checkbox')
-.attr('class','switch is-info is-small');
+  switchEl.append('input')
+  .attr('type','checkbox')
+  .attr('class','switch is-info is-small');
 
-switchEl.append('label');
+  switchEl.append('label');
 
-//Add options
-groupsEnter.append('div').attr('class','tabs is-fullwidth is-toggle'); 
+  //Add options
+  groupsEnter.append('div').attr('class','tabs is-fullwidth is-toggle'); 
 
-let ul = groupsEnter.select('.tabs')
-.append('ul');
+  let ul = groupsEnter.select('.tabs')
+  .append('ul');
 
-groups = groupsEnter.merge(groups);
+  groups = groupsEnter.merge(groups);
 
-//Set data dependent attributes
+  //Set data dependent attributes
 
-groups
-.attr('class',d=>'dataDiv data_' + d);
+  groups
+  .attr('class',d=>'dataDiv data_' + d);
 
-groups.select('.switch')
-.attr('id',d=>'switch_'+d);
+  groups.select('.switch')
+  .attr('id',d=>'switch_'+d);
 
-groups.select('label')
-.attr('for',d=>'switch_'+d);
+  groups.select('label')
+  .attr('for',d=>'switch_'+d);
 
-groups.select('#header')
-.text(d=>optionsObject[d].label);
+  groups.select('#header')
+  .text(d=>optionsObject[d].label);
 
-groups.select('#icon')
-.attr('class','icon tooltip is-tooltip-multiline')
-.attr('data-tooltip',d=>tooltipDict[d]);
+  groups.select('#icon')
+  .attr('class','icon tooltip is-tooltip-multiline')
+  .attr('data-tooltip',d=>tooltipDict[d]);
 
-groups.select('i')
-.attr('class','fas fa-question-circle has-text-grey');
-
-
-let li = groups.select('ul').selectAll('li').data((d)=>optionsObject[d].options.map(
-option=>{return {category:d,option}}));
-
-let liEnter = li.enter().append('li');
-
-liEnter.append('a').append('span');
-
-li = liEnter.merge(li);
-
-li.attr('class',d=> d.category +  ' ' + d.option);
-
-li.select('span').text(d=>d.option);
+  groups.select('i')
+  .attr('class','fas fa-question-circle has-text-grey');
 
 
-li.on('click',function(d){
- 
-let tabGroup = d3.select((this.parentNode).parentNode);
-let category = tabGroup.data()[0];
+  let li = groups.select('ul').selectAll('li').data((d)=>optionsObject[d].options.map(option=>{return {category:d,option}}));
 
-// console.log('clicked', d);
-let isSelected = d3.select(this).classed('is-active');
+  let liEnter = li.enter().append('li');
 
-if (category !== 'structure'){
-  tabGroup.selectAll('li').classed('is-active',false);
-}
-d3.select(this).classed('is-active',!isSelected);
+  liEnter.append('a').append('span');
 
-//if no elements are selected, set toggle to false;
-selectedTabs = tabGroup.selectAll('.is-active');
+  li = liEnter.merge(li);
 
-d3.select('#switch_' + category)
-.property('checked',selectedTabs.empty()? false : true);
+  li.attr('class',d=> d.category +  ' ' + d.option);
 
-compute_scores(allScores);
+  li.select('span').text(d=>d.option);
+
+  li.on('click',function(d){
+  
+    let tabGroup = d3.select((this.parentNode).parentNode);
+    let category = tabGroup.data()[0];
+
+    // console.log('clicked', d);
+    let isSelected = d3.select(this).classed('is-active');
+
+    // if (category !== 'structure'){
+    //   tabGroup.selectAll('li').classed('is-active',false);
+    // }
+    d3.select(this).classed('is-active',!isSelected);
+
+    //if no elements are selected, set toggle to false;
+    selectedTabs = tabGroup.selectAll('.is-active');
+
+    d3.select('#switch_' + category)
+    .property('checked',selectedTabs.empty()? false : true);
+
+    compute_scores(allScores);
 
 
-//highlight all mini panel buttons for active tabs. 
-selectedTabs = d3.select('#wizard_panel').selectAll('.is-active').each(function(tab){
-  let currentClass = d3.select(this).attr('class').replace('is-active','').trim();
-  let miniButtons = d3.selectAll('.button').filter(function(b){
-      return d3.select(this).attr('class').includes(currentClass);
-   }).classed('is-focused',true);
-})
+    //highlight all mini panel buttons for active tabs. 
+    selectedTabs = d3.select(wizard_panel_select).selectAll('.is-active').each(function(tab){
+      let currentClass = d3.select(this).attr('class').replace('is-active','').trim();
+      let miniButtons = d3.selectAll('.button').filter(function(b){
+          return d3.select(this).attr('class').includes(currentClass);
+      }).classed('is-focused',true);
+    })
 
-});
+  });
 
-d3.selectAll(".switch").on("change", function(d){
+  d3.selectAll(".switch").on("change", function(d){
 
-let tabGroup = d3.select(((this.parentNode).parentNode).parentNode).select('.tabs');
-let category = tabGroup.data()[0];
+    let tabGroup = d3.select(((this.parentNode).parentNode).parentNode).select('.tabs');
+    let category = tabGroup.data()[0];
 
-d3.select(this).property('checked',false); //for now disable turning the toggle on;
+    d3.select(this).property('checked',false); //for now disable turning the toggle on;
 
-tabGroup.selectAll('li')
-.classed('is-active',false);
+    tabGroup.selectAll('li')
+    .classed('is-active',false);
 
-compute_scores(allScores);
+    compute_scores(allScores);
 
-//highlight all mini panel buttons for active tabs. 
-selectedTabs = d3.select('#wizard_panel').selectAll('.is-active').each(function(tab){
-  let currentClass = d3.select(this).attr('class').replace('is-active','').trim();
-  let miniButtons = d3.selectAll('.button').filter(function(b){
-      return d3.select(this).attr('class').includes(currentClass);
-   }).classed('is-focused',true);
-})
+    //highlight all mini panel buttons for active tabs. 
+    selectedTabs = d3.select(wizard_panel_select).selectAll('.is-active').each(function(tab){
+      let currentClass = d3.select(this).attr('class').replace('is-active','').trim();
+      let miniButtons = d3.selectAll('.button').filter(function(b){
+          return d3.select(this).attr('class').includes(currentClass);
+      }).classed('is-focused',true);
+    })
 
-});
+  });
 
-compute_scores(allScores);
+  compute_scores(allScores);
 }
 
 function create_mini_panel(techniques,allScores){
@@ -145,45 +143,44 @@ function create_mini_panel(techniques,allScores){
   
   let cards = d3.selectAll('.techniqueCard').data(techniques.map(t=>t[0]));
 
+  let groups = cards.select('#mini_wizard_panel').selectAll('div').data(d=>Object.keys(optionsObject).map(k=>{return {'technique':d,'dimension':k}}));
 
-let groups = cards.select('#mini_wizard_panel').selectAll('div').data(d=>Object.keys(optionsObject).map(k=>{return {'technique':d,'dimension':k}}));
+  let groupsEnter = groups.enter().append('div');
 
-let groupsEnter = groups.enter().append('div');
+  //Add Header
+  let h4 = groupsEnter.append('h6');
 
-//Add Header
-let h4 = groupsEnter.append('h6');
+  h4.append('span').attr('id','header');
 
-h4.append('span').attr('id','header');
+  groups = groupsEnter.merge(groups);
 
-groups = groupsEnter.merge(groups);
-
-//Set data dependent attributes
-
-
-groups.select('#header')
-.text(d=>optionsObject[d.dimension].shortLabel ? optionsObject[d.dimension].shortLabel : optionsObject[d.dimension].label);
+  //Set data dependent attributes
 
 
-let li = groups.selectAll('a').data((d)=>optionsObject[d.dimension].options.map(
-option=>{return {category:d.dimension,option,technique:d.technique}}));
-
-let liEnter = li.enter().append('a');
-
-liEnter.append('a').append('span');
-
-li = liEnter.merge(li);
-
-li.attr('class',d=>{
-  let score = allScores[d.technique][d.category][d.option];
-  return d.category +  ' ' + d.option + ' ' + score2class[score] + ' button tooltip';
-})
-.attr('data-tooltip',d=>d.option);
+  groups.select('#header')
+  .text(d=>optionsObject[d.dimension].shortLabel ? optionsObject[d.dimension].shortLabel : optionsObject[d.dimension].label);
 
 
-li.text(d=>{
-  let score = allScores[d.technique][d.category][d.option];
-  return score;
-});
+  let li = groups.selectAll('a').data((d)=>optionsObject[d.dimension].options.map(
+  option=>{return {category:d.dimension,option,technique:d.technique}}));
+
+  let liEnter = li.enter().append('a');
+
+  liEnter.append('a').append('span');
+
+  li = liEnter.merge(li);
+
+  li.attr('class',d=>{
+    let score = allScores[d.technique][d.category][d.option];
+    return d.category +  ' ' + d.option + ' ' + score2class[score] + ' button tooltip';
+  })
+  .attr('data-tooltip',d=>d.option);
+
+
+  li.text(d=>{
+    let score = allScores[d.technique][d.category][d.option];
+    return score;
+  });
 }
 
 function compute_scores(allScores){
@@ -274,7 +271,7 @@ function render_techniques(techniques,info) {
    cards.select('.scoreZero')
    .style('display',d=> info[d[0]].bad.length === 0 ?  'none' : 'block');
   
-   create_mini_panel(techniques,info);
+  //  create_mini_panel(techniques,info);
 
 
 }
